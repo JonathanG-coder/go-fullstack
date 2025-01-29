2cd9476965b66cc535fd1d23d88e016a92715224
Jonathan <jonathangager75@gmail.com>
2025-01-29 13:16:24 +0100

    Saving Things in the database

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+const Thing = require('../backend/models/Thing');
 
 const app = express();
 
@@ -23,10 +26,13 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Objet créé !'
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
     });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+        .catch(error => res.status(400).json({ error }));
 });
 
 app.get('/api/stuff', (req, res, next) => {
