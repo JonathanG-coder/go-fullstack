211f70b51d59f75b006d723072649151d990d6d0
Jonathan <jonathangager75@gmail.com>
2025-02-07 09:33:20 +0100

    Configuring authentication routes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://JOHN:Falguiere75015@jonathan.ne4tt.mongodb.net/?retryWrites=true&w=majority&appName=Jonathan',
     {
@@ -26,5 +27,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
